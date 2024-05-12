2734d810f2e2023a320d56ff3030e10d30ee6674
Adenola Omotayo <adenolaomotayo@gmail.com>
2024-05-12 15:06:17 +0100

    initial commit

--- src/Pages/PageNotFound.jsx
+++ src/Pages/PageNotFound.jsx
@@ -1,10 +1,17 @@
+import Footer from "../ui/Footer";
+
 function PageNotFound() {
   return (
-    <div>
-      <h1 className="page-header">
-        Page Not Found. Kindly check to confirm if the url is correctly spelt 😢.
-      </h1>
-    </div>
+    <>
+      <div className="page">
+        <h1 className="page-header">
+          Page Not Found. Kindly check to confirm if the url is correctly spelt
+          😢.
+        </h1>
+      </div>
+
+      <Footer />
+    </>
   );
 }
 
